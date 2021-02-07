c4e9da69a7448beb21341dcce596cac4f6585c9f
Oğuz Karan <oguzkaran@csystem.org>
2021-02-07 12:55:37 +0300

    Java-Jan-2021

--- src/eclipse-workspace/Sample/src/csd/App.java
+++ src/eclipse-workspace/Sample/src/csd/App.java
@@ -1,16 +1,11 @@
 /*----------------------------------------------------------------------------------------------------------------------
-	safdsdfgfdhfgjgfhj
+	
 ----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {
-	public static void main(String[]args) 
+	public static void main(String[] args)
 	{
-		System.out.println("Hello, World"); //Burası derleyici tarafından görülmeyecek
+		System.out.println("Hello, World");
 	}
-
-	/*
-		Bu bölge de derleyici tarafından 
-		görülmeyecek		
-	*/
 }
