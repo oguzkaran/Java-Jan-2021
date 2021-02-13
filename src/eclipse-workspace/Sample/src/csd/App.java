b3d4391bfe18271b476b0c09379916ce72aa9f66
Oğuz Karan <oguzkaran@csystem.org>
2021-02-13 12:48:09 +0300

    Java-Jan-2021

--- src/eclipse-workspace/Sample/src/csd/App.java
+++ src/eclipse-workspace/Sample/src/csd/App.java
@@ -1,11 +1,28 @@
 /*----------------------------------------------------------------------------------------------------------------------
+	Tür (type): Bir değişken için bellekte ne kadar yer ayrılacağını ve içerisindeki bilginin hangi formatta tutulacağınıu
+	belirten kavrama denir
 	
+	Temel Türler (built-in/primitive types)
+	Tür İsmi			Uzunluğu (byte)
+	short					2
+   *int						4
+	long					8
+	byte					1
+	
+	float					4
+   *double					8
+	
+	char 					2
+	boolean					-	
 ----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {
 	public static void main(String[] args)
-	{
-		System.out.println("Hello, World");
+	{		
+		boolean a = false;
+		
+		System.out.println(ch);
 	}
 }
+
