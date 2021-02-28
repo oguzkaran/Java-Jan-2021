3f06fb88be48ea30d031dfc59b0a561e8c9e5684
Oğuz Karan <oguzkaran@csystem.org>
2021-02-28 11:01:00 +0300

    Java-Jan-2021

--- src/eclipse-workspace/Sample/src/csd/App.java
+++ src/eclipse-workspace/Sample/src/csd/App.java
@@ -1,16 +1,23 @@
 /*----------------------------------------------------------------------------------------------------------------------
-	 Gerçek sayı sabitlerinde tamsayı kısmının değeri sıfırsa noktadan önce sıfır yazılmayabilir. Aynı durum 
-	 noktadan sonraki sıfır ise de geçerlidir
+	Sınıf Çalışması: Parametresi ile aldığı int türden 3 basamaklı bir sayının basamakları toplamını döngü kullanmadan
+	döndüren digitsSum metodunu yazınız. Metot basamak sayısı kontrolü yapmayacaktır
 ----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {
 	public static void main(String[] args)
-	{	
-		double a = .2;
-		double b = 2.;
+	{
+		java.util.Scanner kb = new java.util.Scanner(System.in);
+		System.out.print("3 basamklı bir sayı giriniz:");
+		int a = Integer.parseInt(kb.nextLine());
 		
-		System.out.printf("a = %f%n", a);
-		System.out.printf("b = %f%n", b);
+		System.out.printf("%d sayısının basamakları toplamı:%d%n", a, NumberUtil.digitsSum(a));			
+	}
+}
+
+class NumberUtil {
+	public static int digitsSum(int val)
+	{		
+		return Math.abs(val / 100 + val % 100 / 10 + val % 10);
 	}
 }
