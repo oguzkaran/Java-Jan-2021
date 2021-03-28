282757533e045bdc41e6cc874ec489518fefed54
Oğuz Karan <oguzkaran@csystem.org>
2021-03-28 12:44:22 +0300

    Java-Jan-2021

--- src/eclipse-workspace/Sample/src/csd/App.java
+++ src/eclipse-workspace/Sample/src/csd/App.java
@@ -1,14 +1,22 @@
 /*----------------------------------------------------------------------------------------------------------------------
-	Gerçek sayı türlerinden tamsayı türlerine doğrudan atama geçersizdir 
+	İşlem öncesi otomatik tür dönüşümü için derleyici geçici değişken yaratılacak kodu üretir. Yani bu dönüşümler
+	geçici değişken yaratılarak yapılır. Derleyicinin aşağıdaki örnek için ürettiği kod yaklaşık şu şekilde yazılabilir:
+	int temp1 = a;
+	int temp2 = b;
+	c = temp1 + temp2;
 ----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {
 	public static void main(String[] args)
 	{		
-		float a = 10;
-		long b;
+		short a = 10;
+		char b = 'Z';		
+		int c;
 		
-		b = a; //error		
+		c = a + b;
+		
+		System.out.printf("c = %d%n", c);
 	}
 }
+
