ee0a83d22a1b89484ca24ad93868893ec2bf605b
Oğuz Karan <oguzkaran@csystem.org>
2021-04-04 12:40:36 +0300

    Java-Jan-2021

--- src/eclipse-workspace/Sample/src/csd/App.java
+++ src/eclipse-workspace/Sample/src/csd/App.java
@@ -1,54 +1,14 @@
 /*----------------------------------------------------------------------------------------------------------------------	
-	Homework-001-3. sorunun bir çözümü
-	(Ödevin verildiği zamanda bilinen konular ile yazılmıştır)
+	Aşağıdaki örnekte uygun abs metodu bulunmadığından error oluşur
 ----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {
 	public static void main(String[] args)
-	{		
-		SignumTest.run();	
+	{
+		boolean a = true;
+		
+		Math.abs(a); //error
 	}
 }
-
-class SignumTest {
-	public static void display(int a)
-	{
-		int result = NumberUtil.signum(a);
-		
-		if (result > 0)
-			System.out.println("Pozitif");
-		else if (result == 0)
-			System.out.println("Sıfır");
-		else
-			System.out.println("Negatif");
-	}
-	
-	public static void run() 
-	{
-		java.util.Scanner kb = new java.util.Scanner(System.in); 
-		
-		System.out.print("Bir sayı giriniz:");
-		int a = Integer.parseInt(kb.nextLine());		
-		
-		display(a);		
-	}
-}
-
-
-class NumberUtil {
-	public static int signum(int a)
-	{
-		if (a > 0)
-			return 1;
-		
-		if (a == 0)
-			return 0;
-		
-		return -1;
-	}
-}
-
-
-
 
