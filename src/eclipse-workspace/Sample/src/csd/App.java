0c25f7cbf6982435a8a56ef5e4beb601b711080d
Oğuz Karan <oguzkaran@csystem.org>
2021-04-10 12:52:00 +0300

    Java-Jan-2021

--- src/eclipse-workspace/Sample/src/csd/App.java
+++ src/eclipse-workspace/Sample/src/csd/App.java
@@ -1,14 +1,32 @@
 /*----------------------------------------------------------------------------------------------------------------------	
-	Aşağıdaki örnekte uygun abs metodu bulunmadığından error oluşur
+	Sınıf türünden bellekte ayrılan bölgelere nesne (object) denir. Nesne ilgili sınıf türünden bir örnek olduğundan 
+	"instance" terimi de kullanılır. Java'da referans bildirimi nesnenin yaratıldığı anlamına gelmez. Nesneler 
+	stack'de yaratılmaz. Heap' de yaratılır. Nesne yaratılması new operatörü yapılır (şüphesiz akış new operatörüne
+	geldiğinde nesne yaratılacak kod üretilir.) new operatörü tek operandlı, önek durumunda bir operatördür. Ürettiği
+	değer heap'de yaratılan nesnenin adresidir(referansıdır.) new operatörünün genel biçimi:
+	
+		new <referans tür ismi>([argümanlar]);
+	Bir nesnenin adresi bir referansa atandığında artık o "referans ilgili nesneyi gösterir" duruma gelir.  
 ----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {
 	public static void main(String[] args)
 	{
-		boolean a = true;
+		Sample s; 
+		Mample m;
 		
-		Math.abs(a); //error
+		
+		s = new Sample();
+		m = new Mample();
+		//...									
 	}
 }
 
+class Sample {
+	//...
+}
+
+class Mample {
+	//...
+}
