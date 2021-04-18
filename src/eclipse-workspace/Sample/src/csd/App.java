6a464c2aca864d241933d24436e8dca3f16854a1
Oğuz Karan <oguzkaran@csystem.org>
2021-04-18 12:33:23 +0300

    Java-Jan-2021

--- src/eclipse-workspace/Sample/src/csd/App.java
+++ src/eclipse-workspace/Sample/src/csd/App.java
@@ -1,69 +1,24 @@
 /*----------------------------------------------------------------------------------------------------------------------	
-	 Point sınıfı ve test kodu
+	Java'da nesne yaratılması adımları:
+	1. Bellekte yer ayrılır
+	2. non-static (final olmayan) veri elemanlarına default değerler atanır
+	3. Başlangıç metodu (constructor [ctor]) çağrılır
+	
+	Bu üç adımın sonunda new operatörünün ürettiği değer yani nesnenin adresi (referansı) elde edilmiş olur
 ----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {
 	public static void main(String[] args)
 	{
-		Point p1, p2;
+		Sample s;
 		
-		p1 = new Point();
-		p2 = new Point();
-		
-		p1.x = 100;
-		p1.y = 200;
-		
-		p2.x = 50;
-		p2.y = -45;
-		
-		p1.display();
-		p2.display();
-		
-		p1.offset(30);
-		p2.offset(10);
-		
-		System.out.println("---------------------");
-		
-		p1.display();
-		p2.display();		
+		s = new Sample();
 	}
 }
 
-class Point {
-	public int x;
-	public int y;
-	
+class Sample {
 	//...
-	
-	public double distance()
-	{
-		return distance(0, 0);				
-	}
-	
-	public double distance(Point other)
-	{
-		return distance(other.x, other.y);
-	}
-	
-	public double distance(int a, int b)
-	{
-		return Math.sqrt((x - a) * (x - a) + (y - b) * (y - b));
-	}
-	
-	public void offset(int dxy)
-	{
-		offset(dxy, dxy);
-	}
-	
-	public void offset(int dx, int dy)
-	{
-		x += dx;
-		y += dy;
-	}	
-	
-	public void display()
-	{
-		System.out.printf("{x: %d, y: %d}%n", x, y);
-	}
 }
+
+
