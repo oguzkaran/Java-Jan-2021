8a7287dc7f2c1604f706115d9844acc7c5163155
Oğuz Karan <oguzkaran@csystem.org>
2021-04-25 12:38:29 +0300

    Java-Jan-2021

--- src/eclipse-workspace/Sample/src/csd/App.java
+++ src/eclipse-workspace/Sample/src/csd/App.java
@@ -1,11 +1,14 @@
 /*----------------------------------------------------------------------------------------------------------------------	
-	Complex sınıfı ve test kodu
+	Aşağıdaki örnekte println metodu birleştirilmiş yazıyı argüman olarak alarak ekrana basar
 ----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {
 	public static void main(String[] args)
-	{
-								
+	{	
+		double ratio = 23.4;		
+		
+		System.out.println("Oran:%" + ratio);
 	}
 }
+
