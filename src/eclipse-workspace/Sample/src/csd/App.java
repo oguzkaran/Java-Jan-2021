fd3d5ae5938a7e4fad950dc74dc0d3822023f8b4
Oğuz Karan <oguzkaran@csystem.org>
2021-05-01 10:30:41 +0300

    Java-Jan-2021

--- src/eclipse-workspace/Sample/src/csd/App.java
+++ src/eclipse-workspace/Sample/src/csd/App.java
@@ -1,14 +1,41 @@
 /*----------------------------------------------------------------------------------------------------------------------	
-	Aşağıdaki örnekte println metodu birleştirilmiş yazıyı argüman olarak alarak ekrana basar
+	Sınıf Çalışması: Klavyeden elma girilene kadar alınan yazıların arasında klavyeden girilen bir yazı (ayraç) olacak 
+	şekilde birleştirilmiş bir String oluşturan programı yazınız
+	Örneğin: ankara, istanbul, izmir, zonguldak girildiğinde
+	ankara-istanbul-izmir-zonguldak
+	yazısı elde edilmelidir
 ----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {
 	public static void main(String[] args)
 	{	
-		double ratio = 23.4;		
-		
-		System.out.println("Oran:%" + ratio);
+		ConcatWithHyphenApp.run();		
 	}
 }
 
+class ConcatWithHyphenApp {
+	public static void run()
+	{
+		java.util.Scanner kb = new java.util.Scanner(System.in);
+		System.out.print("Ayracı giriniz:");
+		String sep = kb.nextLine();		
+		String str = "";
+		
+		for (;;) {
+			System.out.print("Bir yazı giriniz:");
+			String s = kb.nextLine();
+			
+			if (s.trim().isEmpty())
+				continue;
+			
+			if ("elma".equals(s))
+				break;		
+			
+			str += s + sep;			
+		}	
+		
+		str = str.substring(0, str.length() - sep.length());
+		System.out.println(str);
+	}
+}
