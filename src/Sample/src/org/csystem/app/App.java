1ed8a30436982b236c42f4e09b4471ad07d12e5c
Oğuz Karan <oguzkaran@csystem.org>
2021-07-24 12:34:58 +0300

    Java-Jan-2021

--- src/Sample/src/org/csystem/app/App.java
+++ src/Sample/src/org/csystem/app/App.java
@@ -1,11 +1,24 @@
 /*----------------------------------------------------------------------------------------------------------------------
-
+    Object sınıfı her sınıfın doğrudan ya da dolaylı olarak taban sınıfı olduğuna göre her türden referans Object
+    referansına atanabilir. Aşağıdaki örnekte çeşitli referans türleri için Object referansları kullanılarak
+    upcasting yapılmış ve downcasting ile dinamik türlere ilişkin referanslara atama yapılmıştır. Şüphesiz
+    downcasting işleminin dinamik tür bilinemeden yapılması exception oluşumuna yol açabilir. Bilinmiyorsa ne yapılacaktır?
 ----------------------------------------------------------------------------------------------------------------------*/
 package org.csystem.app;
+
+import org.csystem.math.Fraction;
+import org.csystem.util.ArrayUtil;
+import org.csystem.util.datetime.Date;
+
+import java.util.Random;
 
 class App {
     public static void main(String[] args)
     {
+        Object [] objects = new Object[10];
+
+        //...
+
 
     }
 }
