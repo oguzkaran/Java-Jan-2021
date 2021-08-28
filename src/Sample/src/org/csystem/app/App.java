83b452bf4f21cf2cbc427e949ca2b02e7bc8f066
Oğuz Karan <oguzkaran@csystem.org>
2021-08-28 23:22:47 +0300

    Update

--- src/Sample/src/org/csystem/app/App.java
+++ src/Sample/src/org/csystem/app/App.java
@@ -1,53 +1,16 @@
 /*----------------------------------------------------------------------------------------------------------------------
-    Throwable paraemetreli catch bloğu tüm fırlatılan exception nesnelerini yakalayabilir. Şüphesiz Throwable parametreli
-    catch bloğunun tüm catch bloklarından sonra yazılması gerekir
+    Console sınıfı
 ----------------------------------------------------------------------------------------------------------------------*/
 package org.csystem.app;
 
-import java.util.Scanner;
+import org.csystem.util.console.Console;
 
 class App {
     public static void main(String [] args)
     {
-        try {
-            Scanner kb = new Scanner(System.in);
-            System.out.print("Bir sayı giriniz:");
-            double val = Double.parseDouble(kb.nextLine());
+        double a = Console.readDouble("Birinci sayıyı giriniz:", "Hatalı giriş yaptınız:");
+        double b = Console.readDouble("İkinci sayıyı giriniz:", "Hatalı giriş yaptınız:");
 
-            System.out.printf("log(%.2f) = %f%n", val, MathUtil.myLog(val));
-        }
-        catch (YourException ex) {
-            System.out.println("YourException yakalandı");
-        }
-        catch (MyException ex) {
-            System.out.println("MyException yakalandı");
-        }
-        catch (Throwable ex) {
-            System.out.println("Exception yakalandı");
-        }
-
-        System.out.println("Tekrar yapıyor musunuz?");
+        System.out.printf("%f + %f = %f%n", a, b, a + b);
     }
 }
-
-class MathUtil {
-    public static double myLog(double val)
-    {
-        if (val == 0)
-            throw new MyException();
-
-        if (val < 0)
-            throw new YourException();
-
-        return Math.log(val);
-    }
-}
-
-class MyException extends RuntimeException {
-
-}
-
-class YourException extends MyException {
-
-}
-
---
+++ src/Sample/src/org/csystem/util/console/Console.java
@@ -0,0 +1,94 @@
+/*----------------------------------------------------------------------------------------------------------------------
+    Console sınıfı
+----------------------------------------------------------------------------------------------------------------------*/
+package org.csystem.util.console;
+
+import java.util.Scanner;
+
+public final class Console {
+    private static final Scanner KB = new Scanner(System.in);
+
+    private Console()
+    {
+    }
+
+    public static int readInt()
+    {
+        return readInt("");
+    }
+
+    public static int readInt(String message)
+    {
+        return readInt(message, "");
+    }
+
+    public static int readInt(String message, String errMessage)
+    {
+        for (;;) {
+            try {
+                System.out.print(message);
+                return Integer.parseInt(KB.nextLine());
+            }
+            catch (NumberFormatException ignore) {
+                System.out.print(errMessage);
+            }
+        }
+    }
+
+    public static int readIntLine(String message)
+    {
+        return readInt(message + '\n', "");
+    }
+
+    public static int readIntLine(String message, String errMessage)
+    {
+        return readInt(message + '\n', errMessage + '\n');
+    }
+
+    //...
+
+    public static double readDouble()
+    {
+        return readDouble("");
+    }
+
+    public static double readDouble(String message)
+    {
+        return readDouble(message, "");
+    }
+
+    public static double readDouble(String message, String errMessage)
+    {
+        for (;;) {
+            try {
+                System.out.print(message);
+                return Double.parseDouble(KB.nextLine());
+            }
+            catch (NumberFormatException ignore) {
+                System.out.print(errMessage);
+            }
+        }
+    }
+
+    public static double readDoubleLine(String message)
+    {
+        return readDouble(message + '\n');
+    }
+
+    public static double readDoubleLine(String message, String errMessage)
+    {
+        return readDouble(message + '\n', errMessage + '\n');
+    }
+
+    public static String readLine(String message)
+    {
+        return read(message + '\n');
+    }
+
+    public static String read(String message)
+    {
+        System.out.print(message);
+
+        return KB.nextLine();
+    }
+}
