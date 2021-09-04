1b6bf47737896a1a04f20f2899684abb4c518b10
Oğuz Karan <oguzkaran@csystem.org>
2021-09-04 22:28:06 +0300

    Update

--- src/Sample/src/org/csystem/app/App.java
+++ src/Sample/src/org/csystem/app/App.java
@@ -1,5 +1,6 @@
 /*----------------------------------------------------------------------------------------------------------------------
-    Console sınıfı
+    Bir metodun fırlatabileceği checked exception sınıfları arasında türetme ilişkisi varsa throws listesine istenirse
+    yalnızca taban sınıf yazılabilir
 ----------------------------------------------------------------------------------------------------------------------*/
 package org.csystem.app;
 
@@ -8,9 +9,45 @@ import org.csystem.util.console.Console;
 class App {
     public static void main(String [] args)
     {
-        double a = Console.readDouble("Birinci sayıyı giriniz:", "Hatalı giriş yaptınız:");
-        double b = Console.readDouble("İkinci sayıyı giriniz:", "Hatalı giriş yaptınız:");
+        try {
+            double val = Console.readDouble("Bir sayı giriniz:", "Hatalı giriş yaptınız:");
 
-        System.out.printf("%f + %f = %f%n", a, b, a + b);
+            double result = MathUtil.myLog(val);
+
+            System.out.printf("log(%.2f) = %f%n", val, result);
+        }
+        catch (Throwable ex) {
+            System.out.println(ex.getMessage());
+        }
+
+        System.out.println("Tekrar yapıyor musunuz?");
+    }
+}
+
+class MathUtil {
+    public static double myLog(double val) throws MathZeroException
+    {
+        if (val == 0)
+            throw new MathZeroException("val can be zero");
+
+        if (val < 0)
+            throw new MathNegativeException("val can be zero");
+
+        return Math.log(val);
+    }
+}
+
+class MathZeroException extends Exception {
+    public MathZeroException(String message)
+    {
+        super(message);
+    }
+}
+
+
+class MathNegativeException extends MathZeroException {
+    public MathNegativeException(String message)
+    {
+        super(message);
     }
 }
