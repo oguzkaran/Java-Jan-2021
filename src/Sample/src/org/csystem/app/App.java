cbbc586f201e461c5302cc2bf34878bd31daa205
Oğuz Karan <oguzkaran@csystem.org>
2021-09-25 23:04:51 +0300

    Update

--- src/Sample/src/org/csystem/app/App.java
+++ src/Sample/src/org/csystem/app/App.java
@@ -1,85 +1,19 @@
 /*----------------------------------------------------------------------------------------------------------------------
-	Aşağıdaki örnekte devices referansına ilişkin ArrayList nesnesine yeni bir eleman eklenmiştir. clone
-	referansına ilişkin ArrayList nesnesinin bundan haberi olmaz
+	twr bloğu yalnız başına olabilir. Scanner sınıfı Closeable arayüzünü destekler
 ----------------------------------------------------------------------------------------------------------------------*/
 package org.csystem.app;
 
-import java.util.ArrayList;
+import java.util.Scanner;
 
 class App {
     public static void main(String [] args)
     {
-        ArrayList devices = new ArrayList();
+        try (Scanner kb = new Scanner(System.in)) {
+            System.out.print("Bir sayı giriniz:");
+            int val = Integer.parseInt(kb.nextLine());
 
-        devices.add(new DeviceInfo(1, "test", 5050));
-        devices.add(new DeviceInfo(2, "sensor", 5051));
-        devices.add(new DeviceInfo(3, "mest", 5698));
-
-        System.out.printf("devices:%s%n", devices);
-
-        ArrayList clone = (ArrayList)devices.clone();
-
-        System.out.printf("clone:%s%n", clone);
-
-        System.out.println("------------------------------------------------");
-
-        devices.add(new DeviceInfo(5, "weathersensor", 9898));
-
-        System.out.printf("devices:%s%n", devices);
-        System.out.printf("clone:%s%n", clone);
-    }
-}
-
-class DeviceInfo {
-    private int m_id;
-    private String m_name;
-    private int m_port;
-
-    public DeviceInfo() {}
-
-    public DeviceInfo(int id, String name, int port)
-    {
-        //...
-        m_id = id;
-        m_name = name;
-        m_port = port;
-    }
-
-    public int getId()
-    {
-        return m_id;
-    }
-
-    public void setId(int id)
-    {
-        //...
-        m_id = id;
-    }
-
-    public String getName()
-    {
-        return m_name;
-    }
-
-    public void setName(String name)
-    {
-        //...
-        m_name = name;
-    }
-
-    public int getPort()
-    {
-        return m_port;
-    }
-
-    public void setPort(int port)
-    {
-        //...
-        m_port = port;
-    }
-    public String toString()
-    {
-        return String.format("[%d]%s:%d", m_id, m_name, m_port);
+            System.out.println(val * val);
+        }
     }
 }
 
